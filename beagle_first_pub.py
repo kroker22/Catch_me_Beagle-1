f0417f471e558a5cae94da679e44594bdbfd8121
ehdxns <129836561+ehdxns@users.noreply.github.com>
2023-06-06 20:24:42 +0900

    Update beagle_first_pub.py

--- beagle_first_pub.py
+++ beagle_first_pub.py
@@ -26,60 +26,67 @@ class DICE_CHECKER(Node):
         self.publisher_dice4 = self.create_publisher(String, 'topic', 10)
         self.publisher_dice5 = self.create_publisher(String, 'topic', 10)
         self.publisher_dice6 = self.create_publisher(String, 'topic', 10)
+
         self.service_client = self.create_client(
             Beagleposition, 
             'service_name')
+        
         self.service_server = self.create_service(
             Beagleposition, 
             'service_name', 
             self.service_callback)
+        
         self.cam = cam
         self.tmi = tmi
         self.current_dice_label = None
-        timer_period = 0.01  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
 
-        self.action_client = ActionClient(self, Distbeagle, 'beagle_action')
+        timer_period = 0.01  # seconds
+
+        self.action_client = ActionClient(self, Distbeagle, 'distbeagle')  # 액션 클라이언트 생성
         self.goal_handle = None
+
+        self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
         image = self.cam.read()
+
         if self.tmi.predict(image):
             label = self.tmi.get_label()
             conf = self.tmi.get_conf()
             time.sleep(1)
+
             print("Predicted Label:", label)
             print("Confidence:", conf)
+
             if conf > 0.8 and label != self.current_dice_label:
                 self.current_dice_label = label
                 
-                if label == 'DICE1':
-                    
+                if label == 'DICE1':         
                     msg.data = 'DICE1'
                     self.publisher_dice1.publish(msg)
                     self.get_logger().info('Publishing: "DICE1"')
-                elif label == 'DICE2':
-                    
+
+                elif label == 'DICE2':             
                     msg.data = 'DICE2'
                     self.publisher_dice2.publish(msg)
                     self.get_logger().info('Publishing: "DICE2"')
-                elif label == 'DICE3':
-                    
+
+                elif label == 'DICE3':   
                     msg.data = 'DICE3'
                     self.publisher_dice3.publish(msg)
                     self.get_logger().info('Publishing: "DICE3"')
-                elif label == 'DICE4':
-                    
+
+                elif label == 'DICE4':                   
                     msg.data = 'DICE4'
                     self.publisher_dice4.publish(msg)
                     self.get_logger().info('Publishing: "DICE4"')
+
                 elif label == 'DICE5':
-                    
                     msg.data = 'DICE5'
                     self.publisher_dice5.publish(msg)
                     self.get_logger().info('Publishing: "DICE5"')
+
                 elif label == 'DICE6':
-                    
                     msg.data = 'DICE6'
                     self.publisher_dice6.publish(msg)
                     self.get_logger().info('Publishing: "DICE6"')
@@ -90,6 +97,7 @@ class DICE_CHECKER(Node):
 
                     while not self.service_client.wait_for_service(timeout_sec=1.0):
                         self.get_logger().info('Service not available, waiting again...')
+                
                     self.future = self.service_client.call_async(request)
                     self.future.add_done_callback(self.service_response_callback)
 
@@ -104,20 +112,26 @@ class DICE_CHECKER(Node):
         try:
             response = future.result()
         
-            if msg.data == "DICE1" :
+            if msg.data == "DICE1":
                 response.move_point += 1
-            elif msg.data == "DICE2" :
+
+            elif msg.data == "DICE2":
                 response.move_point += 2
-            elif msg.data == "DICE3" :
+
+            elif msg.data == "DICE3":
                 response.move_point += 3
-            elif msg.data == "DICE4" :
+
+            elif msg.data == "DICE4":
                 response.move_point += 4
-            elif msg.data == "DICE5" :
+
+            elif msg.data == "DICE5":
                 response.move_point += 5
-            elif msg.data == "DICE6" :
+
+            elif msg.data == "DICE6":
                 response.move_point += 6
   
             self.get_logger().info('Service Response: %s' % response.move_point)
+
         except Exception as e:
             self.get_logger().info('Service call failed %r' % (e,))
 
@@ -136,7 +150,6 @@ def main(args=None):
 
     dice_checker.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
